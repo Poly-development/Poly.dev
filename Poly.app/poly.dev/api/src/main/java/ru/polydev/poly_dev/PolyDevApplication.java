699e5f25a6971020c43357f224c2c5db272136a7
vladlukynov <lukynov.1@yandex.ru>
2024-10-14 05:26:52 +0300

    Init poly.dev api project

---
+++ Poly.app/poly.dev/api/src/main/java/ru/polydev/poly_dev/PolyDevApplication.java
@@ -0,0 +1,13 @@
+package ru.polydev.poly_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyDevApplication.class, args);
+	}
+
+}
---
+++ Poly.app/poly.dev/api/src/test/java/ru/polydev/poly_dev/PolyDevApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.polydev.poly_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
